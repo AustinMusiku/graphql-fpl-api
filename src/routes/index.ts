296efc996525b692f83cfc62a73ad880e41583fd
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-21 01:27:47 +0300

    Feat: Added type definitions, Loaded schemas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,35 +1,34 @@
-import { Application } from 'express';
-import { createYoga } from 'graphql-yoga';
-import dataLoader from 'dataloader';
+import { Application } from 'express'
+import dataLoader from 'dataloader'
+import { createYoga } from 'graphql-yoga'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 
-import Schema from '../schema';
-const fetchMethods = require('../controllers/fetchControllers');
+// import Schema from '../schema';
+import fetchMethods from '../controllers/fetchControllers'
 
-let getPlayerEventsByIds = async (keys: readonly number[]) => {
-	return keys.map(fetchMethods.getPlayerEventsById)
-}
+const typeDefs = loadSchemaSync('./src/schema/typeDefs/*.graphql', {
+	loaders: [new GraphQLFileLoader()]
+})
 
-let getPlayerDataByIds = async (keys: readonly number[]) => {
-	return keys.map(fetchMethods.getPlayerDataById)
-}
-
-const loaders = {
-	playerEvent: new dataLoader(getPlayerEventsByIds),
-	playerData: new dataLoader(getPlayerDataByIds)
-}
-
-const yoga = createYoga({ 
-	schema: Schema,
+const yoga = createYoga({
+	schema: typeDefs,
 	context: {
-		loaders: loaders
+		loaders: {
+			playerEvent: new dataLoader(async (keys: readonly number[]) => {
+				return keys.map(fetchMethods.getPlayerEventsById)
+			}),
+			playerData: new dataLoader(async (keys: readonly number[]) => {
+				return keys.map(fetchMethods.getPlayerDataById)
+			})
+		}
 	}
 })
 
-
 class Routes {
 	public mountGraphQL(_express: Application): Application {
-		return _express.use('/graphql', yoga);
+		return _express.use('/graphql', yoga)
 	}
 }
 
-export default new Routes;
+export default new Routes()
