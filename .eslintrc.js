fec958dfe2fa487936bfe5feba15531b8ed4f3b5
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-21 01:14:42 +0300

    Chore: Setup linting and formatter, modified tsconfig options

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		'prettier/prettier': ['error']
+	},
+	overrides: [
+		{
+			files: ['*.graphql'],
+			parser: '@graphql-eslint/eslint-plugin',
+			plugins: ['@graphql-eslint']
+		}
+	]
+}
