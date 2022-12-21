76f4ca2263cb4ae9c0974b5c3b2efbfe5d9ac0c8
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-22 02:21:09 +0300

    Fix: Removed url config

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -1,26 +1,24 @@
-import { Application } from 'express';
-import * as path from 'path';
-import * as dotenv from 'dotenv';
+import { Application } from 'express'
+import * as path from 'path'
+import * as dotenv from 'dotenv'
 
 class Locals {
 	public static config(): any {
-		dotenv.config({ path: path.join(__dirname, '../../.env') });
+		dotenv.config({ path: path.join(__dirname, '../../.env') })
 
-		const url = process.env.APP_URL || `http://localhost:${process.env.PORT}`;
-		const port = process.env.PORT || 4600;
-		const appSecret = process.env.APP_SECRET;
+		const port = process.env.PORT || 4500
+		const appSecret = process.env.APP_SECRET
 
 		return {
 			appSecret,
-			port,
-			url
-		};
+			port
+		}
 	}
 
-	public static init (_express: Application): Application {
-		_express.locals.app = this.config();
-		return _express;
+	public static init(_express: Application): Application {
+		_express.locals.app = this.config()
+		return _express
 	}
 }
 
-export default Locals;
+export default Locals
