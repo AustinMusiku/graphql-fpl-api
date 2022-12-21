09c084c75c503ce5653722af8312cfb1c720912b
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-21 04:54:02 +0300

    Feat: Added schema resolvers

--- src/providers/Express.ts
+++ src/providers/Express.ts
@@ -1,40 +1,43 @@
-import express, { Application } from 'express';
+import express, { Application } from 'express'
 
-import Locals from './Locals';
-import Routes from '../routes';
-import Bootstrap from '../middlewares/Kernel';
+import Locals from './Locals'
+import Routes from '../routes'
+import Bootstrap from '../middlewares/Kernel'
 
 class Express {
-	public express: Application;
+	public express: Application
 
-	constructor () {
-		this.express = express();
+	constructor() {
+		this.express = express()
 
-		this.mountDotEnv();
-		this.mountMiddlewares();
-		this.mountRoutes();
+		this.mountDotEnv()
+		this.mountMiddlewares()
+		this.mountRoutes()
 	}
 
-	private mountDotEnv (): void {
-		this.express = Locals.init(this.express);
+	private mountDotEnv(): void {
+		this.express = Locals.init(this.express)
 	}
 
-	private mountMiddlewares (): void {
-		this.express = Bootstrap.init(this.express);
+	private mountMiddlewares(): void {
+		this.express = Bootstrap.init(this.express)
 	}
 
-	private mountRoutes (): void {
-		this.express = Routes.mountGraphQL(this.express);
+	private mountRoutes(): void {
+		this.express = Routes.mountYoga(this.express)
 	}
 
-	public init (): any {
-		const port: number = this.express.locals.app.port;
+	public init(): any {
+		const port: number = this.express.locals.app.port
 
 		// Start the server
 		this.express.listen(port, () => {
-			return console.log('\x1b[33m%s\x1b[0m', `Server :: Running @ 'http://localhost:${port}'`);
+			return console.log(
+				'\x1b[33m%s\x1b[0m',
+				`Server :: Running @ 'http://localhost:${port}'`
+			)
 		})
 	}
 }
 
-export default new Express();
+export default new Express()
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,32 +1,31 @@
 import { Application } from 'express'
 import dataLoader from 'dataloader'
 import { createYoga } from 'graphql-yoga'
-import { loadSchemaSync } from '@graphql-tools/load'
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 
-// import Schema from '../schema';
-import fetchMethods from '../controllers/fetchControllers'
-
-const typeDefs = loadSchemaSync('./src/schema/typeDefs/*.graphql', {
-	loaders: [new GraphQLFileLoader()]
-})
-
-const yoga = createYoga({
-	schema: typeDefs,
-	context: {
-		loaders: {
-			playerEvent: new dataLoader(async (keys: readonly number[]) => {
-				return keys.map(fetchMethods.getPlayerEventsById)
-			}),
-			playerData: new dataLoader(async (keys: readonly number[]) => {
-				return keys.map(fetchMethods.getPlayerDataById)
-			})
-		}
-	}
-})
+import schemaWithResolvers from '../schema'
+import fetchControllers from '../controllers/fetchControllers'
 
 class Routes {
-	public mountGraphQL(_express: Application): Application {
+	public mountYoga(_express: Application): Application {
+		const yoga = createYoga({
+			schema: schemaWithResolvers,
+			context: {
+				loaders: {
+					playerEvent: new dataLoader(
+						async (keys: readonly number[]) => {
+							return keys.map(
+								fetchControllers.getPlayerEventsById
+							)
+						}
+					),
+					playerData: new dataLoader(
+						async (keys: readonly number[]) => {
+							return keys.map(fetchControllers.getPlayerDataById)
+						}
+					)
+				}
+			}
+		})
 		return _express.use('/graphql', yoga)
 	}
 }
