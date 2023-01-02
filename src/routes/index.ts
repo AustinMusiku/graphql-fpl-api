78490c51ba0790f3f048be635aeed2ab5aba4693
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-01-03 01:20:55 +0300

    Fix: Fix typeDefs not in dist - update routes schema

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Application } from 'express'
 import { createYoga } from 'graphql-yoga'
 
-import schemaWithResolvers from '../schema'
+import schema from '../schema'
 import { loaders } from '../loaders'
 
 class Routes {
 	public mountYoga(_express: Application): Application {
 		const yoga = createYoga({
-			schema: schemaWithResolvers,
+			schema: schema,
 			context: {
 				loaders
 			}
