e8f10c5271077056dfd0bbdbf4558cc568f6e0bd
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-01-03 03:45:12 +0300

    Fix: Fixed reading from undefined loaders error

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,15 +1,23 @@
 import dataLoader from 'dataloader'
 import fetchController from '../controllers/fetchControllers'
 
-function loader(controller) {
-	new dataLoader(async (keys: readonly number[]) => {
-		return keys.map(controller)
-	})
-}
-
 export const loaders = {
-	playerEvent: loader(fetchController.getPlayerEventsById),
-	teamUpcomingFixtures: loader(fetchController.getFixturesByTeam),
+	playerEvent: new dataLoader(async (keys: readonly number[]) => {
+		return keys.map(async (key) => {
+			return await fetchController.getPlayerEventsById.call(
+				fetchController,
+				key
+			)
+		})
+	}),
+	teamUpcomingFixtures: new dataLoader(async (keys: readonly number[]) => {
+		return keys.map(async (key) => {
+			return await fetchController.getFixturesByTeam.call(
+				fetchController,
+				key
+			)
+		})
+	}),
 	playerData: new dataLoader(async (keys: readonly number[]) => {
 		return keys.map(async (key) => {
 			return await fetchController.getPlayerDataById.call(
