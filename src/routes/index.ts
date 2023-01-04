2b013632c8ae68f3a9ae61745eadd90c0b140756
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-01-05 01:55:54 +0300

    Feat: Added default route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,17 @@ import schema from '../schema'
 import { loaders } from '../loaders'
 
 class Routes {
+	// default route
+	public default(_express: Application): Application {
+		return _express.get('/', (req, res) => {
+			// return html page with link to graphql playground
+			res.send(`
+				<h1>GraphQL Playground</h1>
+				<a href="/graphql">Click here to go to GraphQL Playground</a>
+			`)
+		})
+	}
+
 	public mountYoga(_express: Application): Application {
 		const yoga = createYoga({
 			schema: schema,
