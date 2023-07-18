ecb087f4b00cb329ffbf5df5c759240da828a8e1
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-07-18 05:17:56 +0300

    feat: Serve static files in public dir

--- src/middlewares/Http.ts
+++ src/middlewares/Http.ts
@@ -1,21 +1,23 @@
-import cors from 'cors';
-import { Application } from 'express';
-import compress from 'compression';
+import cors from 'cors'
+import express, { Application } from 'express'
+import compress from 'compression'
 
 class Http {
 	public static mount(_express: Application): Application {
-
 		// Disable the x-powered-by header in response
-		_express.disable('x-powered-by');
+		_express.disable('x-powered-by')
 
 		// Enables the CORS
-		_express.use(cors());
+		_express.use(cors())
 
 		// Enables the "gzip" / "deflate" compression for response
-		_express.use(compress());
+		_express.use(compress())
 
-		return _express;
+		// serve static files
+		_express.use(express.static('public'))
+
+		return _express
 	}
 }
 
-export default Http;
+export default Http
