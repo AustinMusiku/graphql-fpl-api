46e1146c4cbb2414cdef928270cc36e958d3f48d
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-08-27 14:26:37 +0300

    Feat: Added missing resolvers for `Player`

--- src/resolvers/player.ts
+++ src/resolvers/player.ts
@@ -3,7 +3,6 @@ import { GraphQLError } from 'graphql'
 
 export const Player: PlayerResolvers = {
 	assists: ({ assists }) => assists,
-
 	bonus: ({ bonus }) => bonus,
 	bps: ({ bps }) => bps,
 	chance_of_playing_next_round: ({ chance_of_playing_next_round }) =>
@@ -11,9 +10,27 @@ export const Player: PlayerResolvers = {
 	chance_of_playing_this_round: ({ chance_of_playing_this_round }) =>
 		chance_of_playing_this_round,
 	clean_sheets: ({ clean_sheets }) => clean_sheets,
+	clean_sheets_per_90: ({ clean_sheets_per_90 }) => clean_sheets_per_90,
 	code: ({ code }) => code,
-	creativity: ({ creativity }) => creativity,
+	corners_and_indirect_freekicks_order: ({
+		corners_and_indirect_freekicks_order
+	}) => corners_and_indirect_freekicks_order,
+	corners_and_indirect_freekicks_text: ({
+		corners_and_indirect_freekicks_text
+	}) => corners_and_indirect_freekicks_text,
 	cost_change_event: ({ cost_change_event }) => cost_change_event,
+	cost_change_event_fall: ({ cost_change_event_fall }) =>
+		cost_change_event_fall,
+	cost_change_start: ({ cost_change_start }) => cost_change_start,
+	cost_change_start_fall: ({ cost_change_start_fall }) =>
+		cost_change_start_fall,
+	creativity: ({ creativity }) => creativity,
+	creativity_rank: ({ creativity_rank }) => creativity_rank,
+	creativity_rank_type: ({ creativity_rank_type }) => creativity_rank_type,
+	direct_freekicks_order: ({ direct_freekicks_order }) =>
+		direct_freekicks_order,
+	direct_freekicks_text: ({ direct_freekicks_text }) => direct_freekicks_text,
+	dreamteam_count: ({ dreamteam_count }) => dreamteam_count,
 	element_type: ({ element_type }, _, { loaders }) => {
 		const elementTypeData = loaders.elementTypeData.load(element_type)
 		return elementTypeData
@@ -22,37 +39,79 @@ export const Player: PlayerResolvers = {
 	ep_this: ({ ep_this }) => (ep_this ? parseFloat(ep_this) : null),
 	event_points: ({ event_points }) => event_points,
 	expected_assists: ({ expected_assists }) => expected_assists,
+	expected_assists_per_90: ({ expected_assists_per_90 }) =>
+		expected_assists_per_90,
+	expected_goal_involvements: ({ expected_goal_involvements }) =>
+		expected_goal_involvements,
+	expected_goal_involvements_per_90: ({
+		expected_goal_involvements_per_90
+	}) => expected_goal_involvements_per_90,
 	expected_goals: ({ expected_goals }) => expected_goals,
 	expected_goals_conceded: ({ expected_goals_conceded }) =>
 		expected_goals_conceded,
-	expected_goal_involvements: ({ expected_goal_involvements }) =>
-		expected_goal_involvements,
+	expected_goals_conceded_per_90: ({ expected_goals_conceded_per_90 }) =>
+		expected_goals_conceded_per_90,
+	expected_goals_per_90: ({ expected_goals_per_90 }) => expected_goals_per_90,
 	first_name: ({ first_name }) => first_name,
 	form: ({ form }) => form,
+	form_rank: ({ form_rank }) => form_rank,
+	form_rank_type: ({ form_rank_type }) => form_rank_type,
+	goals_conceded: ({ goals_conceded }) => goals_conceded,
+	goals_conceded_per_90: ({ goals_conceded_per_90 }) => goals_conceded_per_90,
 	goals_scored: ({ goals_scored }) => goals_scored,
 	ict_index: ({ ict_index }) => ict_index,
+	ict_index_rank: ({ ict_index_rank }) => ict_index_rank,
+	ict_index_rank_type: ({ ict_index_rank_type }) => ict_index_rank_type,
 	id: ({ id }) => id,
+	in_dreamteam: ({ in_dreamteam }) => in_dreamteam,
 	influence: ({ influence }) => influence,
+	influence_rank: ({ influence_rank }) => influence_rank,
+	influence_rank_type: ({ influence_rank_type }) => influence_rank_type,
 	minutes: ({ minutes }) => minutes,
 	news: ({ news }) => news,
 	news_added: ({ news_added }) => news_added,
 	now_cost: ({ now_cost }) => now_cost,
-	points_per_game: ({ points_per_game }) => points_per_game,
+	now_cost_rank: ({ now_cost_rank }) => now_cost_rank,
+	now_cost_rank_type: ({ now_cost_rank_type }) => now_cost_rank_type,
+	own_goals: ({ own_goals }) => own_goals,
 	pastFixtures: async ({ id }, _, { loaders }) => {
 		const playerEvents = await loaders.playerEvent.load(id)
 		return playerEvents.history
 	},
-	second_name: ({ second_name }) => second_name,
+	penalties_missed: ({ penalties_missed }) => penalties_missed,
+	penalties_order: ({ penalties_order }) => penalties_order,
+	penalties_saved: ({ penalties_saved }) => penalties_saved,
+	penalties_text: ({ penalties_text }) => penalties_text,
+	photo: ({ photo }) => photo,
+	points_per_game: ({ points_per_game }) => points_per_game,
+	points_per_game_rank: ({ points_per_game_rank }) => points_per_game_rank,
+	points_per_game_rank_type: ({ points_per_game_rank_type }) =>
+		points_per_game_rank_type,
+	red_cards: ({ red_cards }) => red_cards,
 	saves: ({ saves }) => saves,
+	saves_per_90: ({ saves_per_90 }) => saves_per_90,
+	second_name: ({ second_name }) => second_name,
 	selected_by_percent: ({ selected_by_percent }) => selected_by_percent,
-	total_points: ({ total_points }) => total_points,
-	transfers_in_event: ({ transfers_in_event }) => transfers_in_event,
-	transfers_out_event: ({ transfers_out_event }) => transfers_out_event,
-	threat: ({ threat }) => threat,
+	selected_rank: ({ selected_rank }) => selected_rank,
+	selected_rank_type: ({ selected_rank_type }) => selected_rank_type,
+	special: ({ special }) => special,
+	squad_number: ({ squad_number }) => squad_number,
+	starts: ({ starts }) => starts,
+	starts_per_90: ({ starts_per_90 }) => starts_per_90,
+	status: ({ status }) => status,
 	team: async ({ team }, _, { loaders }) => {
 		const teamData = await loaders.teamData.load(team)
 		return teamData
 	},
+	team_code: ({ team_code }) => team_code,
+	threat: ({ threat }) => threat,
+	threat_rank: ({ threat_rank }) => threat_rank,
+	threat_rank_type: ({ threat_rank_type }) => threat_rank_type,
+	total_points: ({ total_points }) => total_points,
+	transfers_in: ({ transfers_in }) => transfers_in,
+	transfers_in_event: ({ transfers_in_event }) => transfers_in_event,
+	transfers_out: ({ transfers_out }) => transfers_out,
+	transfers_out_event: ({ transfers_out_event }) => transfers_out_event,
 	UpcomingFixtures: async (
 		{ team },
 		{ gw, first, last, from, to },
@@ -85,5 +144,8 @@ export const Player: PlayerResolvers = {
 
 		return fixtures
 	},
-	web_name: ({ web_name }) => web_name
+	value_form: ({ value_form }) => value_form,
+	value_season: ({ value_season }) => value_season,
+	web_name: ({ web_name }) => web_name,
+	yellow_cards: ({ yellow_cards }) => yellow_cards
 }
